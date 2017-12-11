240e88d7bda19e93de0b82ccbc0edbbf65ebc8dd
Mohamed Jubair <mjubair@avallain.com>
2017-12-11 12:09:56 +0400

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div style={styles}>
+        <h2>React with ParcelJS</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
